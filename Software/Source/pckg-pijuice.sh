eeb76e70d79b34c8d6c93177bf950d9ca626cb07
Denis Khrutsky <denis.xpy@gmail.com>
2018-01-10 20:41:04 +0600

    Version 1.1

--- Software/Source/pckg-pijuice.sh
+++ Software/Source/pckg-pijuice.sh
@@ -8,10 +8,10 @@ fi
 
 python setup.py --command-packages=stdeb.command bdist_deb
 
-mkdir -p ./deb_dist/pijuice-1.0/bin/
-cp -a ./bin/. ./deb_dist/pijuice-1.0/bin/
+mkdir -p ./deb_dist/pijuice-1.1/bin/
+cp -a ./bin/. ./deb_dist/pijuice-1.1/bin/
 
-cp -a ./debian/. ./deb_dist/pijuice-1.0/debian/
+cp -a ./debian/. ./deb_dist/pijuice-1.1/debian/
 
 #debuild -us -uc
-(cd ./deb_dist/pijuice-1.0 && dpkg-buildpackage -b -rfakeroot -us -uc)
+(cd ./deb_dist/pijuice-1.1 && dpkg-buildpackage -b -rfakeroot -us -uc)
--- Software/Source/pijuice.py
+++ Software/Source/pijuice.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-__version__ = "1.0.1"
+__version__ = "1.1"
 from smbus import SMBus
 import time
 import datetime
