23ae86e93918326758fcefafb1c590775be412de
Ton van Overbeek <tvoverbeek@gmail.com>
2018-08-17 20:29:21 +0200

    Add missing 'import os' to wakeup_enable.py

--- Software/Test/wakeup_enable.py
+++ Software/Test/wakeup_enable.py
@@ -3,7 +3,7 @@
 # This script is started at reboot by cron
 # Since the start is very early in the boot sequence we wait for the i2c-1 device
 
-import pijuice, time
+import pijuice, time, os
 
 while not os.path.exists('/dev/i2c-1'):
     time.sleep(0.1)
