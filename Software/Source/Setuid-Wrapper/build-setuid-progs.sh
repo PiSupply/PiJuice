909255b48981dbe257a0fd05afb657877db744d7
Ton van Overbeek <tvoverbeek@gmail.com>
2018-08-31 20:05:54 +0200

    Correct build-setuid-progs script. Add binaries to bin

--- Software/Source/Setuid-Wrapper/build-setuid-progs.sh
+++ Software/Source/Setuid-Wrapper/build-setuid-progs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-FULL_PATH=/usr/bin/pijuice_gui.py gcc -Wall -o pijuice_gui setuid-prog.c
-FULL_PATH=/usr/bin/pijuice_cli.py gcc -Wall -o pijuice_cli setuid-prog.c
+gcc -DFULL_PATH=\"/usr/bin/pijuice_gui.py\" -Wall -o pijuice_gui setuid-prog.c
+gcc -DFULL_PATH=\"/usr/bin/pijuice_cli.py\" -Wall -o pijuice_cli setuid-prog.c
