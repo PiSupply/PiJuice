6970b1211c36b84e27ec7c13dfcce6c1a7760e91
GarageTech101 <francesco.vannini@happylot.net>
2019-03-24 11:41:47 +0100

    First Commit NatureJuice

---
+++ MakerKits/Nature-Juice/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get install pijuice-base -y
+
+sudo apt-get install git i2c-tools python3-smbus python3-urwid python3-picamera python3-pil fonts-freefont-ttf -y
+
+git clone https://github.com/PiSupply/PiJuice.git
+
+cd ~/PiJuice/MakerKits/Nature-Juice
+
+sudo cp naturejuice.py naturejuice.sh /usr/local/bin/
+sudo cp naturejuice.service /etc/systemd/system/
+sudo systemctl enable naturejuice.service
+
+sudo reboot
