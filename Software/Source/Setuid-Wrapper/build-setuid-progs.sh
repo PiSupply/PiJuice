f6d5f1c49b0e7d8e8ee5403c8a04f3020555f890
Ton van Overbeek <tvoverbeek@gmail.com>
2020-08-02 21:16:26 +0200

    Update 64-bit instructions
    See issue #433 Make sure the executables (setuid wrappers and pijuiceboot)  are rebuild for 64-bit

--- Software/Source/Setuid-Wrapper/build-setuid-progs.sh
+++ Software/Source/Setuid-Wrapper/build-setuid-progs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 gcc -DFULL_PATH=\"/usr/bin/pijuice_gui.py\" -Wall -o pijuice_gui setuid-prog.c
 gcc -DFULL_PATH=\"/usr/bin/pijuice_cli.py\" -Wall -o pijuice_cli setuid-prog.c
+# Also build pijuiceboot so all programs are build for the current architecture (32 or 64 bit)
+gcc -Wall -o pijuiceboot ../../../Firmware/pijuiceboot.c
