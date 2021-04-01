7191dca3315fec4e1d735d3cf1578c69a02eacff
thesteg <steggall.j@gmail.com>
2021-04-01 10:59:54 +0100

    Moved out pwm table to own header. Refactored init routines for led.

--- Firmware/Sources-V1.5_2021_02_06/Src/led.c
+++ Firmware/Sources-V1.5_2021_02_06/Src/led.c
@@ -12,13 +12,11 @@
 #include "util.h"
 
 #include "led.h"
-
+#include "led_pwm_table.h"
 
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim15;
 extern TIM_HandleTypeDef htim17;
-GPIO_InitTypeDef GPIO_InitStruct;
-
 
 typedef struct
 {
@@ -60,163 +58,38 @@ static Led_T leds[LED_COUNT] =
 };
 
 
-static const uint16_t pwm_table[256u] =
+static const uint8_t m_ledFunctionParams[2u][4u] =
 {
-     65535,    65508,    65479,    65451,    65422,    65394,    65365,    65337,
-     65308,    65280,    65251,    65223,    65195,    65166,    65138,    65109,
-     65081,    65052,    65024,    64995,    64967,    64938,    64909,    64878,
-     64847,    64815,    64781,    64747,    64711,    64675,    64637,    64599,
-     64559,    64518,    64476,    64433,    64389,    64344,    64297,    64249,
-     64200,    64150,    64099,    64046,    63992,    63937,    63880,    63822,
-     63763,    63702,    63640,    63577,    63512,    63446,    63379,    63310,
-     63239,    63167,    63094,    63019,    62943,    62865,    62785,    62704,
-     62621,    62537,    62451,    62364,    62275,    62184,    62092,    61998,
-     61902,    61804,    61705,    61604,    61501,    61397,    61290,    61182,
-     61072,    60961,    60847,    60732,    60614,    60495,    60374,    60251,
-     60126,    59999,    59870,    59739,    59606,    59471,    59334,    59195,
-     59053,    58910,    58765,    58618,    58468,    58316,    58163,    58007,
-     57848,    57688,    57525,    57361,    57194,    57024,    56853,    56679,
-     56503,    56324,    56143,    55960,    55774,    55586,    55396,    55203,
-     55008,    54810,    54610,    54408,    54203,    53995,    53785,    53572,
-     53357,    53140,    52919,    52696,    52471,    52243,    52012,    51778,
-     51542,    51304,    51062,    50818,    50571,    50321,    50069,    49813,
-     49555,    49295,    49031,    48764,    48495,    48223,    47948,    47670,
-     47389,    47105,    46818,    46529,    46236,    45940,    45641,    45340,
-     45035,    44727,    44416,    44102,    43785,    43465,    43142,    42815,
-     42486,    42153,    41817,    41478,    41135,    40790,    40441,    40089,
-     39733,    39375,    39013,    38647,    38279,    37907,    37531,    37153,
-     36770,    36385,    35996,    35603,    35207,    34808,    34405,    33999,
-     33589,    33175,    32758,    32338,    31913,    31486,    31054,    30619,
-     30181,    29738,    29292,    28843,    28389,    27932,    27471,    27007,
-     26539,    26066,    25590,    25111,    24627,    24140,    23649,    23153,
-     22654,    22152,    21645,    21134,    20619,    20101,    19578,    19051,
-     18521,    17986,    17447,    16905,    16358,    15807,    15252,    14693,
-     14129,    13562,    12990,    12415,    11835,    11251,    10662,    10070,
-     9473,    8872,    8266,    7657,    7043,    6424,    5802,    5175,
-     4543,    3908,    3267,    2623,    1974,    1320,    662,    0
+		{NV_LED_FUNC_1, NV_LED_PARAM_R_1, NV_LED_PARAM_G_1, NV_LED_PARAM_B_1},
+		{NV_LED_FUNC_2, NV_LED_PARAM_R_2, NV_LED_PARAM_G_2, NV_LED_PARAM_B_2},
 };
-
 
 
 static void LED_SetRGBLedPtr(Led_T * const p_led, const uint8_t r, const uint8_t g, const uint8_t b);
 static void LED_ProcessBlink(Led_T * const p_led, const uint32_t sysTime);
+static void LED_InitFunction(const uint8_t ledIdx);
 
 
 void LED_Init(const uint32_t sysTime)
 {
-	uint16_t var = 0;
-	Led_T * p_led;
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+	HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim17, TIM_CHANNEL_1);
 
-	// Start channel 1 RED2
-	if (HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1) != HAL_OK)
+	LED_InitFunction(LED_LED1_IDX);
+	LED_InitFunction(LED_LED2_IDX);
+
+
+	if (leds[LED_LED1_IDX].func == LED_USER_LED)
 	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	// Start channel 2 Green2
-	if (HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2) != HAL_OK)
-	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	// Start channel 3 Blue2
-	if (HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3) != HAL_OK)
-	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	// LED2 C1
-	if (HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_1) != HAL_OK)
-	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	// LED2 C2
-	if (HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_2) != HAL_OK)
-	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	// LED2 C3
-	if (HAL_TIM_PWM_Start(&htim17, TIM_CHANNEL_1) != HAL_OK)
-	{
-	// PWM generation Error
-	Error_Handler();
-	}
-
-	p_led = &leds[LED_LED1_IDX];
-
-	EE_ReadVariable(NV_LED_FUNC_1, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->func = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_R_1, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramR = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_G_1, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramG = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_B_1, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramB = var&0xFF;
-	}
-
-	if (p_led->func == LED_USER_LED)
-	{
-		LED_SetRGB(LED_LED1_IDX, p_led->paramR, p_led->paramG, p_led->paramB);
+		LED_SetRGBLedPtr(&leds[LED_LED1_IDX], leds[LED_LED1_IDX].paramR, leds[LED_LED1_IDX].paramG, leds[LED_LED1_IDX].paramB);
 	}
 	else
 	{
-		LED_SetRGB(LED_LED1_IDX, 0u, 0u, 0u);
-	}
-
-	p_led = &leds[LED_LED2_IDX];
-	var = 0;
-
-	EE_ReadVariable(NV_LED_FUNC_2, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->func = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_R_2, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramR = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_G_2, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramG = var&0xFF;
-	}
-
-	EE_ReadVariable(NV_LED_PARAM_B_2, &var);
-
-	if (((~var)&0xFF) == (var>>8))
-	{
-		p_led->paramB = var&0xFF;
+		LED_SetRGBLedPtr(&leds[LED_LED1_IDX], 0u, 0u, 0u);
 	}
 }
 
@@ -270,9 +143,6 @@ uint8_t LED_GetParamB(uint8_t func)
 		return 0u;
 	}
 }
-
-
-
 
 
 void LED_Service(const uint32_t sysTime)
@@ -348,98 +218,17 @@ void LedStart(void)
 
 void LED_SetConfigurationData(const uint8_t ledIdx, const uint8_t * const data, const uint8_t len)
 {
-	uint16_t var = 0;
-
 	if (ledIdx > LED_LAST_LED_IDX)
 	{
 		return;
 	}
 
-	if (ledIdx == 0u)
-	{
-		NvWriteVariableU8(NV_LED_FUNC_1, data[0]);
-		NvWriteVariableU8(NV_LED_PARAM_R_1, data[1]);
-		NvWriteVariableU8(NV_LED_PARAM_G_1, data[2]);
-		NvWriteVariableU8(NV_LED_PARAM_B_1, data[3]);
+	NvWriteVariableU8(m_ledFunctionParams[ledIdx][0u], data[0u]);
+	NvWriteVariableU8(m_ledFunctionParams[ledIdx][1u], data[1u]);
+	NvWriteVariableU8(m_ledFunctionParams[ledIdx][2u], data[2u]);
+	NvWriteVariableU8(m_ledFunctionParams[ledIdx][3u], data[3u]);
 
-		EE_ReadVariable(NV_LED_FUNC_1, &var);
-
-		if (((~var)&0xFF) == (var>>8)) {
-			leds[ledIdx].func = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_R_1, &var);
-
-		if (((~var)&0xFF) == (var>>8)) {
-			leds[ledIdx].paramR = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_G_1, &var);
-
-		if (((~var)&0xFF) == (var>>8)) {
-			leds[ledIdx].paramG = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_B_1, &var);
-
-		if (((~var)&0xFF) == (var>>8)) {
-			leds[ledIdx].paramB = var&0xFF;
-		}
-
-		if (leds[ledIdx].func == LED_USER_LED)
-		{
-			LED_SetRGBLedPtr(&leds[ledIdx], leds[ledIdx].paramR, leds[ledIdx].paramG, leds[ledIdx].paramB);
-		}
-		else
-		{
-			LED_SetRGB(ledIdx, 0u, 0u, 0u);
-		}
-	}
-
-	if (ledIdx == 1u)
-	{
-		NvWriteVariableU8(NV_LED_FUNC_2, data[0]);
-		NvWriteVariableU8(NV_LED_PARAM_R_2, data[1]);
-		NvWriteVariableU8(NV_LED_PARAM_G_2, data[2]);
-		NvWriteVariableU8(NV_LED_PARAM_B_2, data[3]);
-
-		EE_ReadVariable(NV_LED_FUNC_2, &var);
-
-		if (((~var)&0xFF) == (var>>8))
-		{
-			leds[ledIdx].func = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_R_2, &var);
-
-		if (((~var)&0xFF) == (var>>8))
-		{
-			leds[ledIdx].paramR = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_G_2, &var);
-
-		if (((~var)&0xFF) == (var>>8))
-		{
-			leds[ledIdx].paramG = var&0xFF;
-		}
-
-		EE_ReadVariable(NV_LED_PARAM_B_2, &var);
-
-		if (((~var)&0xFF) == (var>>8))
-		{
-			leds[ledIdx].paramB = var&0xFF;
-		}
-
-		if (leds[ledIdx].func == LED_USER_LED)
-		{
-			LED_SetRGBLedPtr(&leds[ledIdx], leds[ledIdx].paramR, leds[ledIdx].paramG, leds[ledIdx].paramB);
-		}
-		else
-		{
-			LED_SetRGBLedPtr(&leds[ledIdx], 0, 0, 0);
-		}
-	}
+	LED_InitFunction(ledIdx);
 }
 
 
@@ -594,4 +383,48 @@ void LED_SetRGBLedPtr(Led_T * const p_led, const uint8_t r, const uint8_t g, con
 	*p_led->pwmDrv_r = (UINT16_MAX - pwm_table[r]);
 	*p_led->pwmDrv_g = (UINT16_MAX - pwm_table[g]);
 	*p_led->pwmDrv_b = (UINT16_MAX - pwm_table[b]);
+}
+
+
+void LED_InitFunction(const uint8_t ledIdx)
+{
+	Led_T * const p_led = &leds[ledIdx];
+	uint16_t var = 0;
+
+	EE_ReadVariable(m_ledFunctionParams[ledIdx][0u], &var);
+
+	if (UTIL_NV_ParamInitCheck_U16(var))
+	{
+		p_led->func = (LedFunction_T)(var & 0xFFu);
+	}
+
+	EE_ReadVariable(m_ledFunctionParams[ledIdx][1u], &var);
+
+	if (UTIL_NV_ParamInitCheck_U16(var))
+	{
+		p_led->paramR = (uint8_t)(var & 0xFFu);
+	}
+
+	EE_ReadVariable(m_ledFunctionParams[ledIdx][2u], &var);
+
+	if (UTIL_NV_ParamInitCheck_U16(var))
+	{
+		p_led->paramG = (uint8_t)(var & 0xFFu);
+	}
+
+	EE_ReadVariable(m_ledFunctionParams[ledIdx][3u], &var);
+
+	if (UTIL_NV_ParamInitCheck_U16(var))
+	{
+		p_led->paramB = (uint8_t)(var & 0xFFu);
+	}
+
+	if (p_led->func == LED_USER_LED)
+	{
+		LED_SetRGBLedPtr(p_led, p_led->paramR, p_led->paramG, p_led->paramB);
+	}
+	else
+	{
+		LED_SetRGBLedPtr(p_led, 0u, 0u, 0u);
+	}
 }
